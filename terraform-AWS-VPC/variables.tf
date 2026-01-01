0e5bd4e79ca41e935204398118c1c47d3bc94a91
shahanariaz2022 <110471537+shahanariaz2022@users.noreply.github.com>
2026-01-01 08:22:42 -0600

    Update variables.tf

--- terraform-AWS-VPC/variables.tf
+++ terraform-AWS-VPC/variables.tf
@@ -1,1 +1,9 @@
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
 
+variable "Availability_zones" {
+    type = list(string)
+    description = "Availability Zones"
+    default = ["us-east-1a", "us-east-1b"]
+}
